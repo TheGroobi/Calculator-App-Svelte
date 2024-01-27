7711fc15df60ee62efa6af0862374e68b52cbd17
Szymon W <23wilk.szymon@gmail.com>
2024-01-27 01:46:07 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<svelte:head>
+    <title>Calculator App Svelte</title>
+    <meta name="description" content="Svelte demo app" />
+    <link rel="icon" href="/favicon.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kelly+Slab&family=Syncopate:wght@700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+    import Button from "../routes/Button/+page.svelte";
+</script>
+
+<main class="wrapper">
+    <div class="calculator">
+        <button class="CE">CE</button>
+        <div class="display"><span>128673</span></div>
+        <Button/>
+    </div>
+</main>
+<style>
+    :global(body) {
+        background: #f2f2f2;
+        margin: 0;
+        font-family: --font;
+    }
+    .wrapper {
+        min-height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .calculator {
+        width: 641px;
+        padding: 57px;
+        border-radius: 50px;
+        background: #AAB1BB;
+        height: 779px;
+        margin: auto auto;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 45px;
+    }
+    .display {
+        grid-row: 1;
+        grid-column: 1 / -1;
+        background: #4F6C62;
+        border-radius: 20px;
+        text-align: right;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        max-height: 100px;
+    }
+    .display span{ 
+        font-size: 75px;
+        padding-right: 21px;
+        font-family: 'Kelly Slab', 'sans-serif';
+    }
+    .CE {
+        max-height: 100px;
+        grid-row: 1;
+        grid-column: 1 / 2;
+        border-top-left-radius: 20px;
+        border-bottom-left-radius: 20px;
+        z-index: 1;
+        border: none;
+        font-size: 50px;
+        background: rgba(204, 42, 42, 0.5);
+        color: white;
+    }
+</style>
---
+++ src/routes/Button/+page.svelte
@@ -0,0 +1,28 @@
+
+    {#each [7, 8, 9, '÷', 4, 5, 6, '*', 1, 2, 3, '-', ',', 0, '=', '+'] as item}
+        {#if typeof item === 'number' || item === ','}
+            <button>{item}</button>
+        {:else if item === '='}
+            <button class="equal-btn">{item}</button>
+        {:else}
+            <button class="dark-btn">{item}</button>
+        {/if}
+    {/each}
+<style>
+    button {
+        font-size: 50px;
+        border-radius: 15px;
+        background-color: var(--color, #63778E);
+        color: var(--font-color, rgba(0, 0, 0, 0.55));
+        border: none;
+        font-family: 'Syncopate', 'sans-serif';
+    }
+    .dark-btn {
+        --color: #37424F;
+        --font-color: white;
+    }
+    .equal-btn {
+        --color: #234E81;
+        --font-color: white;
+    }
+</style>
